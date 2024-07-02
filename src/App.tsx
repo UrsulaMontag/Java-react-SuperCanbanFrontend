26a50e1c70e1ca692f0f0adcf17cc073c0e592fa
ugla <montagu666@gmail.com>
2024-07-02 15:37:36 +0200

    Add pages routes and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
 import './App.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import MainLayout from "./layouts/MainLayout.tsx";
+import Home from "./routes/Home.tsx";
+import TodoGallery from "./routes/TodoGallery.tsx";
+import DoingGallery from "./routes/DoingGallery.tsx";
+import DoneGallery from "./routes/DoneGallery.tsx";
 
 function App() {
 
+    const router = createBrowserRouter([
+        {
+            path: '/',
+            element: <MainLayout/>,
+            children: [
+                {
+                    path: '/',
+                    element: <Home/>,
+                },
+                {
+                    path: '/board/todo',
+                    element: <TodoGallery/>
+                }, {
+                    path: '/board/doing',
+                    element: <DoingGallery/>
+                }, {
+                    path: '/board/done',
+                    element: <DoneGallery/>
+                },
+
+            ]
+        }
+    ])
 
     return (
         <>
-
+            <RouterProvider router={router}/>
         </>
     )
 }
---
+++ src/routes/TodoGallery.tsx
@@ -0,0 +1,5 @@
+export default function TodoGallery() {
+    return (
+        <></>
+    )
+}
